b8542881e33dab5fe47632ba0e46a06cc528433d
noreallyimfine <avrahamjacobsohn@gmail.com>
2024-12-10 16:01:03 -0500

    update position of cards but it still sucks

--- src/UploadForm.js
+++ src/UploadForm.js
@@ -61,7 +61,7 @@ const UploadForm = () => {
         {error && <div className="text-red-500 font-bold">{error}</div>}
       </form>
       {winner && <WinnerBanner winner={winner} />}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="flex justify-between items-center mt-8 mx-auto w-3/4">
         {players &&
           Object.entries(players).map(([key, player]) => (
             <PlayerCard key={key} player={player} />
